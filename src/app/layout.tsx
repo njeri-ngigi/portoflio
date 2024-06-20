365174f52c34f84c7611c54f04db4e0584b25993
Shalon N. Ngigi <njery.ngigi@gmail.com>
2024-06-20 22:23:53 +0300

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./styles/globals.css";
 
 export const metadata: Metadata = {
@@ -26,7 +27,10 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
